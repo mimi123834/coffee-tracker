a20ee288dc99f8d0625ddfdb5c99d372596f7d48
mimi123834 <amima.basit@icloud.com>
2025-01-14 12:32:06 +0000

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -29,9 +29,9 @@ export default function Layout(props) {
         </header>
     )
 
-    const footer = (
+  const footer = (
         <footer>
-            <p><span className="text-gradient">Caffiend</span> was made by <a target="_blank" href="https://www.smoljames.com">Smoljames</a> <br />using the <a href="https://www.fantacss.smoljames.com" target="_blank">FantaCSS</a> design library.<br />Check out the project on <a target="_black" href="https://www.github.com/jamezmca/reactjs-full-course">GitHub</a>!</p>
+            <p><span className="text-gradient">Caffiend</span> was made by <a href="https://github.com/mimi123834/coffee-tracker.git">GitHub</a>!</p>
         </footer>
     )
 
